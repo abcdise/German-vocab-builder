2ef3af7ac1912cc2b4dcdd39435b35d28f3ee33b
abcdise <zelin.r.ad@mein.gmx>
2024-07-10 08:04:15 +0200

    refactor: Update config file paths and remove unused files

--- src/PhraseDictionary.py
+++ src/PhraseDictionary.py
@@ -5,7 +5,7 @@ import itertools
 from copy import deepcopy
 from DictionaryReader import phrase_json_path
 
-phrase_json_config_path = '../scr/config/Everyday German/config.json'
+phrase_json_config_path = '../scr/config/Alltagsdeutsch/config.json'
 
 
 class PhraseDictionary:
